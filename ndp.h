8b6e67f4707056e1fc4dc9c912ab88b57b55e5ad
Linus Lüssing <linus.luessing@ascom.ch>
2010-12-09 10:23:53 +0100

    batman-adv: Creating neighbor structures, updating LQs
    It will now be checked if a neighbor discovery packet from a new
    neighbor on a certain interface has been received. If so,
    structures in memory will be allocated for further seqno-tracking
    and RQ calculations, and TQ reception, which will be updated
    frequently from this commit on.
    
    Signed-off-by: Linus Lüssing <linus.luessing@ascom.ch>

--- ndp.h
+++ ndp.h
@@ -4,3 +4,8 @@ void stop_ndp_timer(struct batman_if *batman_if);
 
 int ndp_init(struct batman_if *batman_if);
 void ndp_free(struct batman_if *batman_if);
+
+uint8_t ndp_fetch_tq(struct batman_packet_ndp *packet,
+		 uint8_t *my_if_addr);
+int ndp_update_neighbor(uint8_t my_tq, uint32_t seqno,
+			struct batman_if *batman_if, uint8_t *neigh_addr);
